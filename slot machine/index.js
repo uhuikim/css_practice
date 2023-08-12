920ccbcbf3284386c63d640158c41df28b41acb7
uhuikim <uhui94@naver.com>
2023-08-12 14:52:42 +0900

    feat: background position 애니메이션

--- slot machine/index.js
+++ slot machine/index.js
@@ -1,3 +1,27 @@
 const icon_width = 79,
     icon_height = 79,
-    num_icon = 9;
+    num_icon = 9,
+    time_per_icon = 100,
+    indexes = [0, 0, 0];
+
+const roll = (reel, offset = 0) => {
+    const delta = (offset + 2) * num_icon + Math.round(Math.random() * num_icon);
+
+    const style = getComputedStyle(reel);
+    const backgroundPositionY = parseFloat(style["background-position-y"]);
+
+    console.log(backgroundPositionY);
+
+    reel.style.transition = `background-position-y ${8 + delta * time_per_icon}ms`;
+    reel.style.backgroundPositionY = `${backgroundPositionY + delta * icon_height}px`;
+};
+
+const rollAll = () => {
+    const reelsList = document.querySelectorAll(".slots > .reel");
+
+    [...reelsList].map((reel, index) => {
+        roll(reel, index);
+    });
+};
+
+rollAll();
